f9c11c4f4f61aa3f03076f5a2a59e85d88e94f78
Shahrukh@786 <shahrukh.khan169@gmail.com>
2023-08-03 23:40:29 +0530

    assignment_new code

--- testcases/conftest.py
+++ testcases/conftest.py
@@ -9,13 +9,16 @@ from selenium.webdriver.chrome.service import Service
 
 #Setting up the fixture to run the test script with 2- browsers option (Chrome or Firefox)
 
-@pytest.fixture(params=["chrome"], scope="class")
+@pytest.fixture(params=["Edge"], scope="class")
 def setup(request):
     s = Service("C:\Program Files\JetBrains\chromedriver_win32\chromedriver")
     if request.param == "chrome":
         driver = webdriver.Chrome(service=s)
-    if request.param == "firefox":
-        driver = webdriver.Firefox(service=s)
+
+    s = Service("I:\edgedriver\msedgedriver")
+    if request.param == "Edge":
+        driver = webdriver.Edge(service=s)
+
     driver.get("https://www.amazon.in/?&ext_vrnc=hi&tag=googhydrabk1-21&ref=pd_sl_3c01r1321r_e&adgrpid=60612964962&hvpone=&hvptwo=&hvadid=610714031509&hvpos=&hvnetw=g&hvrand=3857331985147024786&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9301885&hvtargid=kwd-32679660&hydadcr=14454_2316419")
     driver.maximize_window()
     request.cls.driver = driver
